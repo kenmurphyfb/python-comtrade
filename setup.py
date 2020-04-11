2b1d8f847fedbc7fefcec7e67db8799e07b66ee6
kenmurphyfb <63359363+kenmurphyfb@users.noreply.github.com>
2020-04-11 09:26:18 -0400

    Reorganize file struture and prepare for packaging
    Reorganize file structure and create setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'python-comtrade',
+    py_modules = ['comtrade'],
+    version = '0.1',  
+    description = 'A library to import Comtrade waveform file',
+    author = 'kenmurphyfb',
+    author_email = 'kenmurphy@fb.com',
+    url = 'github package source url',
+    download_url = 'https://github.com/kenmurphyfb/python-comtrade/archive/0.1.tar.gz',
+    keywords = ['python', 'comtrade'],
+    classifiers = [],
+)
