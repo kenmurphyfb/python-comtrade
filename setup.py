3b4252f55e075098e217bfeae18cedda4fe4eb79
kenmurphyfb <63359363+kenmurphyfb@users.noreply.github.com>
2020-10-04 10:15:44 -0400

    Up-rev to v0.3
    v0.3 modified the time-stamp regex to allow for non-conformant single-digit minutes.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'python-comtrade',
     py_modules = ['comtrade'],
-    version = '0.2',  
+    version = '0.3',  
     description = 'A library to import Comtrade waveform file',
     author = 'kenmurphyfb',
     author_email = 'kenmurphy@fb.com',
     url = 'github package source url',
-    download_url = 'https://github.com/kenmurphyfb/python-comtrade/archive/0.1.tar.gz',
+    download_url = 'https://github.com/kenmurphyfb/python-comtrade/archive/0.3.tar.gz',
     keywords = ['python', 'comtrade'],
     classifiers = [],
 )
